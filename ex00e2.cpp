cd53f819ee76ed72184692ef4ca92d42b0e14768
Jirawat14 <natchjirawat@gmail.com>
2022-08-11 13:37:06 +0700

    finish ex00e2

--- ex00e2.cpp
+++ ex00e2.cpp
@@ -20,16 +20,16 @@ int main() {
     
     for (int i = 0; i < R; i++) {
         cin >> r1 >> c1 >> r2 >> c2;
-        if (r1 > r2 && c1 > c2) cout << "INVALID" << endl;
+        if (r1 > r2 || c1 > c2) cout << "INVALID" << endl;
         else if (r1 > rows || c1 > cols  || r2 < 0 || c2 < 0) cout << "OUTSIDE" << endl;
         else {
             maxNumber = tbl[r1 - 1][c1 - 1];
-            for (int row = r1 - 1; row < r2 - 1; row++) {
-                for (int col = c1 - 1; col < c2 - 1; col++ ) {
-                    if (row > rows || col > cols) break;
+            for (int row = r1 - 1; row <= r2 - 1; row++) {
+                for (int col = c1 - 1; col <= c2 - 1; col++ ) {
+                    if (row >= rows || col >= cols) break;
                     if (tbl[row][col] > maxNumber) maxNumber = tbl[row][col];
                 }
-                if (row > rows) break;
+                if (row >= rows) break;
             }
             cout << maxNumber << endl;
         }
