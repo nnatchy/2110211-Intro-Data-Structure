fc924f6804965d790c93a0cbe776c7c16388ef54
Jirawat14 <natchjirawat@gmail.com>
2022-08-11 10:45:27 +0700

    30p on ex00e2

--- ex00e1.cpp
+++ ex00e1.cpp
@@ -4,28 +4,17 @@ using namespace std;
 
 int main() {
     int hour, minute, delay;
+    int Hour, Minute;
 
     cin >> hour >> minute;
     cin >> delay;
 
-    if (minute + delay >= 60) {
-        hour += (delay + minute) / 60;
-        minute = (minute + delay) % 60;
-    } else {
-        minute += delay;
-    }
-    
-    if (hour >= 24) {
-        hour %= 24;
-    }
+    Minute = (minute + delay) % 60;
+    Hour = ( hour + ((minute + delay) / 60)) % 24;
 
-    if (hour < 10 && minute < 10) {
-        cout << "0" << hour << " " << "0" << minute << endl;
-    } else if (minute < 10) {
-        cout << hour << " " << "0" << minute << endl;
-    } else if (hour < 10) {
-        cout << "0" << hour << " " << minute << endl;
-    } else {
-        cout << hour << " " << minute << endl;
-    }
+    if (Hour < 10) cout << "0";
+    cout << Hour << " ";
+
+    if (Minute < 10) cout << "0";
+    cout << Minute << "\n";
 }
