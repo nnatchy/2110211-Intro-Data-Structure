a28869c39b9b2aaa156eab4571dd752b57b9d927
Jirawat14 <natchjirawat@gmail.com>
2022-08-15 11:25:24 +0700

    finish 15/08/2022

---
+++ ds00_reverse.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void reverse(vector<int> &v,int a,int b) {
+    //write your code only in this function
+    vector<int> temp = v;
+    int ind = a;
+    for (auto it = temp.begin() + b; it >= temp.begin() + a; it--) {
+        v[ind] = *it;
+        ind++;
+    }
+}
+int main() {
+    //read input
+    int n,a,b;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0;i < n;i++) {
+        int c;
+        cin >> c;
+        v.push_back(c);
+    }
+    cin >> a >> b;
+    //call function
+    reverse(v,a,b);
+    //display content of the vector
+    for (auto &x : v)
+    cout << x << " ";
+    cout << endl;
+}
