7f559f3ce6268f54f427974016be8623f3d3035b
natchy <natchjirawat@gmail.com>
2022-08-20 00:32:00 +0700

    update vector_intersection another method

--- d62_q1c_vector_intersection.cpp
+++ d62_q1c_vector_intersection.cpp
@@ -1,28 +1,53 @@
-#include<iostream>
-#include<set>
-#include<algorithm>
-#include<iterator>
-#include<vector>
+                                                    // TODO First method //
+// #include<iostream>
+// #include<set>
+// #include<algorithm>
+// #include<iterator>
+// #include<vector>
 
-using namespace std;
+// using namespace std;
+
+// int main() {
+
+//     set<int> s1, s2;
+//     vector<int> final;
+//     int size1, size2, inp;
+//     cin >> size1 >> size2;
+//     while (size1--) {
+//         cin >> inp;
+//         s1.insert(inp);
+//     }
+//     while (size2--) {
+//         cin >> inp;
+//         s2.insert(inp);
+//     }
+
+//     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(final));
+
+//     for (auto &x : final) {
+//         cout << x << " ";
+//     }
+// }
+
+                                                    // TODO Second method //
+#include <iostream>
+#include <set>
 
 int main() {
-    set<int> s1, s2;
-    vector<int> final;
     int size1, size2, inp;
+    set<int> s1, s2;
     cin >> size1 >> size2;
+
     while (size1--) {
         cin >> inp;
         s1.insert(inp);
     }
     while (size2--) {
         cin >> inp;
-        s2.insert(inp);
+        if (s1.find(inp) != s1.end()) s2.insert(inp);
     }
-
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(final));
-
-    for (auto &x : final) {
+    for (auto &x : s2) {
         cout << x << " ";
     }
+    cout << endl;
 }
