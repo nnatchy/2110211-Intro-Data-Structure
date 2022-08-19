c72016e20da5fc54ebb9fc051d2df45af59dfa8a
Jirawat14 <natchjirawat@gmail.com>
2022-08-19 13:42:46 +0700

    finish pair_sum

--- ds01_pair_sum.cpp
+++ ds01_pair_sum.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <set>
+
+using namespace std;
 
 int main() {
+    int numberSize, targetSize, inp, target;
+    bool found;
+    set<int> s;
+    cin >> numberSize >> targetSize;
+    for (int i = 0; i < numberSize; i++) {
+        cin >> inp;
+        s.insert(inp);
+    }
     
+    for (int i = 0; i < targetSize; i++) {
+        cin >> target;
+        found = false;
+        for (auto it = s.begin(); it != s.end(); it++) {
+            int result = target - *it;
+            if (s.find(result) != s.end() && result != *it) {
+                found = true;
+                break;
+            }
+        }
+        
+        if (!found) {
+            cout << "NO" << endl;
+            continue;
+        }
+        cout << "YES" << endl;
+    }
 }
