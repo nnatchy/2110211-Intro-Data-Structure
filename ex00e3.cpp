cbef25f5bdd8c32d1f5e6e1a9e723e177a4645eb
Jirawat14 <natchjirawat@gmail.com>
2022-08-22 11:58:22 +0700

    finish ex00e3

---
+++ ex00e3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+int main() {
+    int n, num, temp;
+    vector<int> v;
+    bool increase = false, decrease = false, check = false;
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+    set<int> s(v.begin(), v.end());
+    temp = v[0];
+    for (vector<int>::iterator it = v.begin() + 1; it != v.end(); it++) {
+        if (*it < 1 || *it > n) {check = true; break;}
+        if (*it > temp) increase = true;
+        else if (*it < temp) decrease = true;
+        if (s.size() < v.size()) {
+            check = true; break;
+        } if (increase && decrease && s.size() != v.size()) {
+            check = true; break;
+        }
+        
+    }
+    if (check) cout << "NO" << "\n";
+    else cout << "YES" << "\n";
+}
