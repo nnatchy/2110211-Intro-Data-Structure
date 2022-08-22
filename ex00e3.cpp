db538a06e9d5393332f22d048cdad0c1a6eb2689
Jirawat14 <natchjirawat@gmail.com>
2022-08-22 13:27:35 +0700

    format code

--- ex00e3.cpp
+++ ex00e3.cpp
@@ -1,32 +1,50 @@
-#include<iostream>
-#include<vector>
-#include<set>
+#include <iostream>
+#include <vector>
+#include <set>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n, num, temp;
     vector<int> v;
     bool increase = false, decrease = false, check = false;
-    ios_base::sync_with_stdio(false); cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> num;
         v.push_back(num);
     }
     set<int> s(v.begin(), v.end());
     temp = v[0];
-    for (vector<int>::iterator it = v.begin() + 1; it != v.end(); it++) {
-        if (*it < 1 || *it > n) {check = true; break;}
-        if (*it > temp) increase = true;
-        else if (*it < temp) decrease = true;
-        if (s.size() < v.size()) {
-            check = true; break;
-        } if (increase && decrease && s.size() != v.size()) {
-            check = true; break;
+    for (vector<int>::iterator it = v.begin() + 1; it != v.end(); it++)
+    {
+        if (*it < 1 || *it > n)
+        {
+            check = true;
+            break;
         }
-        
+        if (*it > temp)
+            increase = true;
+        else if (*it < temp)
+            decrease = true;
+        if (s.size() < v.size())
+        {
+            check = true;
+            break;
+        }
+        if (increase && decrease && s.size() != v.size())
+        {
+            check = true;
+            break;
+        }
     }
-    if (check) cout << "NO" << "\n";
-    else cout << "YES" << "\n";
+    if (check)
+        cout << "NO"
+             << "\n";
+    else
+        cout << "YES"
+             << "\n";
 }
