7338c2c8ae8dbb018dc426d575cc10b8c5555175
natchy <natchjirawat@gmail.com>
2022-08-26 18:16:32 +0700

    FINALLY FINISH PAIR SUM !

--- ds01_pair_sum.cpp
+++ ds01_pair_sum.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main() {
+    //std::ios_base::sync_with_stdio(false); std::cin.tie(0); uncomment this for speed
     int n, m, inp;
     map<int,int> dupe;
     set<int> s;
     cin >> n >> m;
     while (n--) {
         cin >> inp;
-        if (dupe.find(inp) != dupe.end()) dupe[inp]++;
-        else dupe[inp] = 1;
+        if (s.find(inp) != s.end()) dupe[inp] = 1;
         s.insert(inp);
     }
     while (m--) {
@@ -22,7 +22,7 @@ int main() {
             if (s.find(inp - *it) != s.end() && inp - *it != *it) {
                 found = true;
                 break;
-            } else if (dupe.find(inp - *it) != dupe.end() && dupe[inp - *it] > 1) {
+            } else if (dupe.find(inp - *it) != dupe.end() && dupe[inp - *it] == 1) {
                 found = true;
                 break;
             }
