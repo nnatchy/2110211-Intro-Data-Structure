7ce367c5faf388228361ec38fe7c08ae872ea5cd
Jirawat14 <natchjirawat@gmail.com>
2022-08-27 07:50:09 +0700

    modify code

--- d63_q1a_hiatus.cpp
+++ d63_q1a_hiatus.cpp
@@ -1,50 +1,30 @@
-#include<iostream>
-#include<map>
-#include<set>
+#include <iostream>
+#include <set>
 
 using namespace std;
 
 int main() {
-    std::ios_base::sync_with_stdio(false); std::cin.tie(0);
-    int n, m, year, month;
-    map<int,int> md;
-    set<pair<int,int>> s;
+    ios_base::sync_with_stdio(false); cin.tie(0); 
+    int n, m, month, year;
     cin >> n >> m;
+    set<pair<int,int> > date;
+    string out;
     while (n--) {
         cin >> year >> month;
-        md[year] = month;
-        s.insert(make_pair(year, month));
+        date.insert(make_pair(year, month));
     }
     while (m--) {
         cin >> year >> month;
-        if (s.find(make_pair(year,month)) != s.end()) {
+        if (date.find(make_pair(year, month)) != date.end()) {
             cout << "0 0 ";
             continue;
-        }
-        auto beg = s.begin();
-        while (true) {
-            if (year < beg->first || year == beg->first && month < beg->second) {
+        } else {
+            auto upper = date.lower_bound(make_pair(year, month));
+            if (upper == date.begin()) {
                 cout << "-1 -1 ";
-                break;
-            }
-
-            if (md.find(year) != md.end()) {
-                if (s.find(make_pair(year,month)) == s.end()) {
-                    month--;
-                }
-
-                if (month == 0) {
-                    month = 12;
-                    year--;
-                }
-
-                if (s.find(make_pair(year,month)) != s.end()) {
-                    cout << year << " " << month << " ";
-                    break;
-                }
             } else {
-                year--;
-                month = 12;
+                upper--;
+                cout << upper->first << " " << upper->second << " ";
             }
         }
     }
