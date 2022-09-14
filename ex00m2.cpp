b217d2296aa705e8f3405432394c32006ff0de52
Jirawat14 <natchjirawat@gmail.com>
2022-09-14 09:59:05 +0700

    ini

--- ex00m2.cpp
+++ ex00m2.cpp
@@ -1,39 +1,72 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+
 #define endl "\n"
 
 using namespace std;
 
+void printVector(vector<map<int,int> > &v) {
+    for (int i = 0; i < v.size(); i++) {
+        for (auto it = v[i].begin(); it != v[i].end(); it++) {
+            cout << "This is i : " << i << " ";
+            cout << it->first << " " << it->second << endl;
+        }
+    }
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    ios::sync_with_stdio(false); cin.tie(0);
     int n, m, a;
-    map<int,int> items;
-    map<int,vector<int> > store; // key = U, value = vector of U bidding
-    map<int,vector<pair<int,int> > > auction; // key = U, value = vector of pair of price and item
+    vector<int> items;
+    
     cin >> n >> m >> a;
     for (int i = 0; i < n; i++) {
         int item;
         cin >> item;
-        items[i] = item;
+        items.push_back(item);
     }
 
+    vector<map<int,int> > vm(n); // key is U and P is value
     while (a--) {
         char type;
         int U, I, P;
         cin >> type;
-        if (type == "B") {
+        if (type == 'B') {
             cin >> U >> I >> P;
-            auction[U].push_back(make_pair(I,P));
-            store[U].push_back(I);
-        } else if (type == "W") {
+            I--;
+            vm[I][U] = P;
+        } else if (type == 'W') {
             cin >> U >> I;
-            auto it = lower_bound(store[U].begin(), store[U].end(), I);
-            store[U].erase(it);
+            I--;
+            vm[I].erase(U);
         }
     }
-    for (int i = 0; i < m; i++) {
-        priority_queue<int> pq;
 
+    //printVector(vm);
+
+    vector<vector<int> > fin(m);
+    priority_queue<pair<int,int> > pq;
+    for (int i = 0; i < m; i++) {
+        for (auto it = vm[i].begin(); it != vm[i].end(); it++) {
+            pq.push(make_pair(it->second,it->first));
+        }
+        while (!pq.empty() && items[i] > 0) {
+            int winner = pq.top().second - 1;
+            fin[winner].push_back(i+1);
+            pq.pop();
+            items[i]--;
+        }
+    }
+
+    for (auto &user : fin) {
+        if (user.size() > 0) {
+            for (int x : user) cout << x << " ";
+        } else {
+            cout << "NONE";
+        }
+        cout << endl;
     }
 }
 
@@ -44,3 +77,4 @@ B 1 1 10
 B 2 1 100
 B 1 2 99
 B 2 2 100
+*/
