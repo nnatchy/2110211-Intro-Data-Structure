41313bc89bc9f7b4b6aa40ddda308a4aa7f02e5d
Jirawat14 <natchjirawat@gmail.com>
2022-09-17 18:08:10 +0700

    got it 100

--- d59_q1_range_insert/student.h
+++ d59_q1_range_insert/student.h
@@ -4,24 +4,38 @@
 template <typename T>
 void CP::vector<T>::insert(iterator position,iterator first,iterator last) {
   //write your code here
+  TODO: First sol;
+  // int insertSize = last - first;
+  // T* arr = new T[mSize + insertSize];
+  // int pos = position - begin();
+  // int i = 0; int j = 0;
+  // while (j < mSize + insertSize) {
+  //   if (i == pos) {
+  //     for (auto it = first; it != last; it++) {
+  //       arr[j] = *it;
+  //       j++;
+  //     }
+  //   }
+  //   arr[j] = mData[i];
+  //   i++; j++;
+  // }
+  // delete[] mData;
+  // mData = arr;
+  // mSize = mSize + insertSize;
+  // mCap = mSize;
+
+  TODO: Second sol;
+  
   int insertSize = last - first;
-  T* arr = new T(mSize + insertSize);
   int pos = position - begin();
-  int i = 0; int j = 0;
-  while (j < mSize + insertSize) {
-    if (i == pos) {
-      for (auto it = first; it != last; it++) {
-        arr[j] = *it;
-        j++;
-      }
-    }
-    arr[j] = mData[i];
-    i++; j++;
+  ensureCapacity(mSize + insertSize);
+  for (int i = mSize - 1; i >= pos; i--) {
+    mData[i + insertSize] = mData[i];
   }
-  delete[] mData;
-  mData = arr;
-  mSize = mSize + insertSize;
-  mCap = mSize;
+  for (auto it = first; it != last; it++) {
+    mData[pos++] = *it;
+  }
+  mSize += insertSize;
 }
 
 #endif
