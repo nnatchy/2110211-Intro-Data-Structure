754f9b183e11de7d79910505824a90ab71948ae7
Jirawat14 <natchjirawat@gmail.com>
2022-09-21 09:32:41 +0700

    atleast i can do this one

--- d58_q1_erase_many/student.h
+++ d58_q1_erase_many/student.h
@@ -6,22 +6,7 @@
 template <typename T>
 void CP::vector<T>::erase_many(const std::vector<int> &pos) {
   //write your code here
-  std::vector<int> v;
-  v = pos;
-  sort(v.begin(), v.end());
-  T* arr = new T[mSize - pos.size()];
-  int x = 0;
-  int i = 0;
-  for (int i = 0; i < mSize - v.size(); i++) {
-    if (i == v[x]) {
-      x++;
-      continue;
-    }
-    arr[i] = mData[i];
-  }
-  delete[] mData;
-  mData = arr;
-  mSize = mSize - pos.size();
+  
 }
 
 #endif
--- d63_q2b_stack_reserve/student.h
+++ d63_q2b_stack_reserve/student.h
@@ -4,6 +4,11 @@
 template <typename T>
 int CP::stack<T>::compare_reserve(const CP::stack<T> &other) const {
     //write your code here
+    int ownLeft = mCap - mSize;
+    int otherLeft = other.mCap - other.mSize;
+    if (ownLeft == otherLeft) return 0;
+    if (ownLeft > otherLeft) return 1;
+    if (ownLeft < otherLeft) return -1; 
 }
 
 #endif
