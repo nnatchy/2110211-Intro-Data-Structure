71fe85bb7cb87702e848f60c69dc9e9c9aea7764
Jirawat14 <natchjirawat@gmail.com>
2022-09-27 15:39:14 +0700

    finiss b2f

---
+++ d63_q3b_queue_b2f/student.h
@@ -0,0 +1,17 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <iostream>
+
+
+template <typename T>
+void CP::queue<T>::back_to_front() {
+  //write your code here
+  ensureCapacity(mSize);
+  mData[(mFront - 1 + mCap) % mCap] = mData[(mSize + mFront - 1) % mCap];
+  mFront = (mFront - 1 + mCap) % mCap;
+}
+
+#endif
+
--- d64_q3a_queue_check.cpp
+++ d64_q3a_queue_check.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#define endl "\n"
 using namespace std;
 
 int main() {
@@ -8,17 +9,32 @@ int main() {
         int mFront, mSize, mCap, last, correction;
         cin >> mFront >> mSize >> mCap >> last >> correction;
         if (correction == 0) {
-            if (mSize > mCap) cout << "WRONG\n";
-            else if (mSize == mCap) {
-                if (last > mCap) cout << "WRONG\n";
-                else if (last != mFront) cout << "WRONG\n";
-            }
-            else {
-                if (last != mSize) cout << "WRONG\n";
-                else cout << "OK\n";
-            }
+            if (mSize > mCap) cout << "WRONG" << endl;
+            else if (last > mCap) cout << "WRONG" << endl;
+            else cout << "OK\n";
         } else if (correction >= 1 && correction <= 4) {
-
+            int corVal = 0;
+            if (correction == 1) {
+                if (mFront + mSize != last) {
+                    corVal = last - mSize;
+                }
+            } else if (correction == 2) {
+                if (mSize + mFront != last) {
+                    corVal = last;
+                }
+            } else if (correction == 3) {
+                if (last >= mSize + mFront) {
+                    corVal = last + 1;
+                } else if (mSize >= last + mFront) {
+                    corVal = mSize;
+                }
+            } else if (correction == 4) {
+                if (mSize + mFront != last) {
+                    corVal = mSize + mFront;
+                }
+            }
+            if (corVal == 0) cout << "OK\n";
+            else cout << "WRONG " << corVal << endl; 
         }
     }
 }
