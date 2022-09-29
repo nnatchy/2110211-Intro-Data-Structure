8bb6dc5ba90e1dd3d02becf765991bf2b885d22e
Jirawat14 <natchjirawat@gmail.com>
2022-09-29 09:30:03 +0700

    finally

---
+++ d63_q3a_queue_m2b/student.h
@@ -0,0 +1,19 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <iostream>
+
+
+template <typename T>
+void CP::queue<T>::move_to_back(size_t pos) {
+  //write your code here
+  T val = mData[(mFront + pos) % mCap];
+  for (int i = pos + 1; i < mSize; i++) {
+    mData[(mFront + i - 1) % mCap] = mData[(mFront + i) % mCap];
+  }
+  mData[(mFront + mSize - 1) % mCap] = val;
+}                  
+
+#endif
+
